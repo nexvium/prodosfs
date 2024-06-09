faa068de4a2063c478149d2a0da4d7319ecf361a
Javier Alvarado <91161958+nexvium@users.noreply.github.com>
2024-06-09 11:04:47 -0700

    Add comments.

--- include/prodos/disk.hxx
+++ include/prodos/disk.hxx
@@ -12,6 +12,10 @@
 namespace prodos
 {
 
+/*
+** The disk class deals only with the physical layout of the disk, sectors and blocks, and 
+** does not know anything about their contents.
+*/
 class disk_t
 {
 public:
@@ -20,15 +24,17 @@ public:
 
     disk_t(const std::string & pathname);
     disk_t(const disk_t &)                  = delete;
-    disk_t(const disk_t &&)                 = delete;
 
     virtual ~disk_t();
 
     disk_t &    operator=(const disk_t &)   = delete;
 
+    // ProDOS works with sequentially numbered blocks, each of which consists of
+    // two not-necessarily sequential sectors.
     const void *    ReadBlock(int index) const;
     void            WriteBlock(int index, const void * block);
 
+    // This returns an individual sector, which is half of some block.
     const void *    ReadTrackSector(int track, int sector) const;
 
     unsigned        NumBlocks(void) const
@@ -36,9 +42,15 @@ public:
         return  _num_blocks;
     }
 
+    // Some disk images are in the older DOS 3.3 track-and-sector format. This converts
+    // the image in memory to the block-addressable format that ProDOS expects.
+    // 
+    // Conversion is only supported in one direction until there's a need for the other.
     enum convert_t { RWTS_TO_BLOCK };
     void Convert(convert_t direction);
 
+    // Given a memory address, this returns the image byte offset, or -1 if not in the image.
+    // Used for logging and debugging.
     ssize_t ToOffset(const void * addr) const;
 
 private:
